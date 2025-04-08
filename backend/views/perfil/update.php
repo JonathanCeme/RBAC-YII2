2d79c87c588eb55efc9a661758b0ed03fc68f13f
JonathanCeme <jonathanceme215@gmail.com>
2025-04-07 23:25:58 -0600

    Modificando las vitas perfil: update--> pag 209

--- backend/views/perfil/update.php
+++ backend/views/perfil/update.php
@@ -5,17 +5,13 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var frontend\models\perfil $model */
 
-$this->title = 'Update Perfil: ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Perfils', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->title = 'Actualizar el Perfil de '. $model->user->username;
+$this->params['breadcrumbs'][] = ['label' => 'Perfil', 'url' => ['index']];
+$this->params['breadcrumbs'][] = 'Actualizar';
 ?>
 <div class="perfil-update">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
-
+<h1><?= Html::encode($this->title) ?></h1>
+<?= $this->render('_form', [
+'model' => $model,
+]) ?>
 </div>
