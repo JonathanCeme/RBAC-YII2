4113e8b58edd1bbd218cdf033f1752c63fd2702b
Asayri Dorantes <asayri29chi@gmail.com>
2025-06-15 21:45:33 -0600

    Agregue el site de la ruta \frontend\views\site

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,16 +1,47 @@
 <?php
-
 /** @var yii\web\View $this */
+
+
+use yii\helpers\Html;
 
 $this->title = 'My Yii Application';
 ?>
+<style>
+    body {
+        background-image: url('https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=1350&q=80');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }
+    .lead{
+    text-align: center;
+}
+.titulo {
+    text-align: center;
+    font-size: 2.5rem;
+    font-weight: bold;
+    color: #333;
+}
+</style>
 <div class="site-index">
-    <div class="p-5 mb-4 bg-transparent rounded-3">
-        <div class="container-fluid py-5 text-center">
-            <h1 class="display-4">Congratulations!</h1>
-            <p class="fs-5 fw-light">You have successfully created your Yii-powered application.</p>
-            <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
-        </div>
+
+    <div class="jumbotron">
+
+        <?php if (Yii::$app->user->isGuest) {
+            echo Html::a('Comience Hoy', ['site/signup'],
+                ['class' => 'btn btn-lg btn-success']);} ?>
+        </p>
+
+
+        <h1 class = "titulo">Yii 2 Build</h1>
+
+        <p class="lead">Use esta plantilla de Yii 2 para comenzar Proyectos.</p>
+
+
+        <br/>
+
+        
+
     </div>
 
     <div class="body-content">
